661430f72830cbc166d83c4bf72006e8974c8f89
Nisarg2104 <nisargvora2104@gmail.com>
2019-10-16 23:10:59 +0530

    d

--- app/src/main/java/com/unic/unicproject01/MainActivity.java
+++ app/src/main/java/com/unic/unicproject01/MainActivity.java
@@ -2,20 +2,27 @@ package com.unic.unicproject01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseFirestore db;
-    private  ProductDetails product;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        db = FirebaseFirestore.getInstance();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,Login.class));
+                finish();
 
-        db.collection("shops").document().collection("products").document(product.getProdId()).set(product);
+            }
+        }, 1500);
+
 
     }
 }
